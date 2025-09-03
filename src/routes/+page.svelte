03c5544592b42e02a7d3f427815afd28f6bb1a82
mrwan2546 <unturnedmc1@gmail.com>
2025-09-03 17:50:47 +0700

    feat: Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,48 @@
+<main
+	class="container mx-auto flex min-h-[calc(100dvh-64px)] flex-row items-center justify-center text-white"
+>
+	<div class="text-center">
+		<h1 class="tracking-in-expand text-6xl font-bold tracking-[12px]">SIRILERK</h1>
+		<h4 class="text-focus-in font-light">Simplicity is the ultimate sophistication.</h4>
+	</div>
+</main>
+
+<footer class="sticky bottom-0">
+	<div class="flex flex-row justify-center gap-4">
+		<a
+			href="https://github.com/mrwan2546"
+			target="_blank"
+			aria-label="GitHub"
+			class="slide-in-blurred-bottom duration-300 hover:scale-[1.1]"
+			style="animation-delay: .2s"
+		>
+			<svg
+				role="img"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+				class="w-[32px] fill-white"
+				><path
+					d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+				/></svg
+			>
+		</a>
+		<a
+			href="https://www.linkedin.com/in/sririeakintakam/"
+			target="_blank"
+			aria-label="Linkedin"
+			class="slide-in-blurred-bottom duration-300 hover:scale-[1.1]"
+			style="animation-delay: .3s"
+		>
+			<svg
+				viewBox="0 0 42 42"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+				class="w-[32px] fill-white"
+			>
+				<path
+					d="M37 0H5C2.24 0 0 2.24 0 5V37C0 39.76 2.24 42 5 42H37C39.76 42 42 39.76 42 37V5C42 2.24 39.76 0 37 0ZM13 16V35H7V16H13ZM7 10.47C7 9.07 8.2 8 10 8C11.8 8 12.93 9.07 13 10.47C13 11.87 11.88 13 10 13C8.2 13 7 11.87 7 10.47ZM35 35H29C29 35 29 25.74 29 25C29 23 28 21 25.5 20.96H25.42C23 20.96 22 23.02 22 25C22 25.91 22 35 22 35H16V16H22V18.56C22 18.56 23.93 16 27.81 16C31.78 16 35 18.73 35 24.26V35Z"
+				/>
+			</svg>
+		</a>
+	</div>
+</footer>
