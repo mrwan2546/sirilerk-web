7292864a212deb1c8e9598be25b182ee80079720
mrwan2546 <unturnedmc1@gmail.com>
2025-09-09 17:09:48 +0700

    feat: Add new repo project

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -7,24 +7,64 @@
 		},
 		{
 			url: 'https://github.com/mrwan2546/promptparse-py',
-			title: 'promptparse-go',
+			title: 'promptparse-py',
 			description: 'All-in-one Python for PromptPay & EMVCo QR Codes'
 		},
 		{
 			url: 'https://github.com/mrwan2546/promptparse-go',
 			title: 'promptparse-go',
 			description: 'All-in-one Golang for PromptPay & EMVCo QR Codes'
+		},
+		{
+			url: 'https://github.com/mrwan2546/storeapi',
+			title: 'storeapi',
+			description: ''
+		},
+		{
+			url: 'https://github.com/mrwan2546/EnkaNetwork.py',
+			title: 'EnkaNetwork.py',
+			description: 'Library for fetching JSON data from site https://enka.network/ with Python'
+		},
+		{
+			url: 'https://github.com/mrwan2546/cfpage',
+			title: 'cfpage',
+			description: 'A simple for deploy Cloudflare page with custom data in .env file'
+		},
+		{
+			url: 'https://github.com/mrwan2546/pyinitgen',
+			title: 'pyinitgen',
+			description: 'A CLI generate init python'
+		},
+		{
+			url: 'https://github.com/mrwan2546/FishTerm',
+			title: 'FishTerm',
+			description: 'A open-soruce Terminal remote access'
+		},
+		{
+			url: 'https://github.com/mrwan2546/RenderPage',
+			title: 'RenderPage',
+			description: 'Pre-render page smailer like prerender.io'
+		},
+		{
+			url: 'https://github.com/mrwan2546/VTracker-Event-Stream-TS',
+			title: 'VTracker-Event-Stream-TS',
+			description: ''
+		},
+		{
+			url: 'https://github.com/mrwan2546/Discord-Tech-Talk',
+			title: 'Discord-Tech-Talk',
+			description: 'Bot discord for make tech-talk like clubhouse but you can not to verify community discord server.'
 		}
 	];
 </script>
 
 <div class="container mx-auto">
-	<div class="mt-6 grid grid-cols-3 gap-4">
+	<div class="mt-6 grid grid-cols-3 gap-6">
 		{#each items as item}
 			<a
 				href={item.url}
 				aria-label={`Showcase ${item.title}, ${item.description} url.`}
-				class="bg-white/5 p-5 flex flex-row gap-3 rounded-md scale-100 hover:scale-[1.03] duration-300 drop-shadow-none hover:drop-shadow-md drop-shadow-white"
+				class="bg-white/5 p-5 flex flex-row gap-3 rounded-md scale-100 hover:scale-[1.02] duration-300 drop-shadow-none hover:drop-shadow-md drop-shadow-white"
 			>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
@@ -44,8 +84,8 @@
 					<path d="M18 9a9 9 0 0 1-9 9" />
 				</svg>
 				<div class="text-white">
-					<h1 class="font-bold text-xl">{item.title}</h1>
-					<p class="text-sm">{item.description || 'nil'}</p>
+					<h1 class="font-bold text-lg">{item.title}</h1>
+					<p class="text-sm">{item.description || '-'}</p>
 				</div>
 			</a>
 		{/each}
